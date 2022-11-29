9321d66cc90e928827d2790d5344e8a07644a3d6
Igor Carvalho <igorr.ackerman@gmail.com>
2022-11-29 16:05:03 -0300

    Cria todos os elementos da tela de login (Requisitos 2 e 3)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,26 @@
+// import { useState } from 'react';
+
+// const INITIAL_USER_STATE = {
+//  email: '',
+// };
+
 function Login() {
+//  const [user, setUser] = useState(INITIAL_USER_STATE);
+
   return (
-    <div/>
+    <div>
+      <input
+        placeholder="Email"
+        type="text"
+        data-testid="email-input"
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        data-testid="password-input"
+      />
+      <button data-testid="login-submit-btn" type="button">Enter</button>
+    </div>
   );
 }
 
