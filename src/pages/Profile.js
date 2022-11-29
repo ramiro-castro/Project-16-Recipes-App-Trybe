129b9fad59405eda0a588110308451e347ee60e9
Josiel da Costa Carvaho <josiel.jcc@hotmail.com>
2022-11-29 19:39:47 -0300

    req 57,58,59 - cria o campo para mostra o email e os botões

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
 function Profile() {
+  const [emailStorage, setEmailStorage] = useState('');
+  useEffect(() => {
+    const { email } = JSON.parse(localStorage.getItem('user'));
+    console.log(email);
+    setEmailStorage(email);
+  }, []);
   return (
     <div>
       <Header profile>Profile</Header>
+      <p data-testid="profile-email">{emailStorage}</p>
+      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
     </div>
   );
 }
