b257f72584dd49e83ecc468acc26d2ae1c81da95
Fernando Barreto <fernando.gbarreto@hotmail.com>
2022-11-30 17:47:58 -0300

    req 19 e 20 sem os testes

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -7,6 +7,8 @@ function Recipes() {
   const [path] = useState(location.pathname);
   const [mealData, setMealData] = useState([]);
   const [drinksData, setDriksData] = useState([]);
+  const [mealCategories, setMealCategories] = useState([]);
+  const [drinkCategories, setDrinkCategories] = useState([]);
 
   useEffect(() => {
     const fetchMeal = async () => {
@@ -26,19 +28,54 @@ function Recipes() {
     fetchMeal();
   }, []);
 
+  useEffect(() => {
+    const mealCategory = async () => {
+      const url = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+      const request = await fetch(url);
+      const response = await request.json();
+      setMealCategories(response.meals);
+    };
+    const drinkCategory = async () => {
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+      const request = await fetch(url);
+      const response = await request.json();
+      setDrinkCategories(response.drinks);
+    };
+    drinkCategory();
+    mealCategory();
+  }, []);
+
   if (path === '/drinks') {
     return (
       <main className="mainRecipes">
         <h1>receitas de drinks</h1>
-        <section>
+        <nav>
+          {drinkCategories.map((drink, index) => index < 5 && (
+            <button
+              data-testid={ `${drink.strCategory}-category-filter` }
+              type="button"
+              key={ index }
+            >
+              {drink.strCategory}
+
+            </button>))}
+        </nav>
+        <section className="sectionRecipes">
           {drinksData.map((drinks, index) => index < 12 && (
             <div key={ index } className="card" data-testid={ `${index}-recipe-card` }>
               <img
                 className="recipeImg"
                 src={ drinks.strDrinkThumb }
                 alt={ drinks.strDrink }
+                data-testid={ `${index}-card-img` }
               />
-              <h3 className="recipeName">{drinks.strDrink}</h3>
+              <h3
+                className="recipeName"
+                data-testid={ `${index}-card-name` }
+              >
+                {drinks.strDrink}
+
+              </h3>
             </div>
           ))}
         </section>
@@ -48,15 +85,33 @@ function Recipes() {
   return (
     <main>
       <h1>receitas de meals</h1>
-      <section>
+      <nav>
+        {mealCategories.map((meal, index) => index < 5 && (
+          <button
+            data-testid={ `${meal.strCategory}-category-filter` }
+            type="button"
+            key={ index }
+          >
+            {meal.strCategory}
+
+          </button>))}
+      </nav>
+      <section className="sectionRecipes">
         {mealData.map((meal, index) => index < 12 && (
           <div key={ index } className="card" data-testid={ `${index}-recipe-card` }>
             <img
               className="recipeImg"
               src={ meal.strMealThumb }
               alt={ meal.strMeal }
+              data-testid={ `${index}-card-img` }
             />
-            <h3 className="recipeName">{meal.strMeal}</h3>
+            <h3
+              className="recipeName"
+              data-testid={ `${index}-card-name` }
+            >
+              {meal.strMeal}
+
+            </h3>
           </div>
         ))}
       </section>
