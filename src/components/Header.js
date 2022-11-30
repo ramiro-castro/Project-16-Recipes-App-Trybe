8b437864053e3cd86286f0d2e52dd1438ca3ae52
Thiago Omoto <108958531+Thromoto@users.noreply.github.com>
2022-11-29 23:13:19 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
