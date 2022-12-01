2907fff93cba2a9e76e6ce460fd9b087f177a753
Thiago Omoto <thi.omoto@gmail.com>
2022-12-01 17:23:56 -0300

    requisito 38 feito

--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
-import Footer from './Footer';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 function RecipeInProgress() {
   const history = useHistory();
@@ -17,7 +18,7 @@ function RecipeInProgress() {
           setCategory('Meal');
         });
     } else {
-      fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=17256')
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${match.params.id}`)
         .then((res) => res.json())
         .then((data) => setRecipe(data.drinks[0]));
       setCategory('Drink');
@@ -25,55 +26,50 @@ function RecipeInProgress() {
   }, []);
 
   const getVideoId = () => recipe.strYoutube.split('v=')[1];
-  const getIngredients = () => Object.entries(recipe)
-    .filter((key) => key[0].includes('strIngredient')).map((ingredient) => ingredient[1]);
+  const getIngredients = () => (Object.entries(recipe)
+    .filter((key) => key[0].includes('strIngredient'))
+    .map((ingredient) => ingredient[1]));
+
   const getMeasures = () => Object.entries(recipe)
     .filter((key) => key[0].includes('strMeasure')).map((ingredient) => ingredient[1]);
 
-  console.log(recipe);
+  let ingred = [];
+  let indexNull = 0;
+
+  if (recipe !== undefined) {
+    ingred = getIngredients();
+    console.log(ingred);
+    console.log(ingred.indexOf(''));
+    indexNull = ingred.indexOf('');
+  }
 
   return (
     <div>
-      <img data-testid="recipe-photo" src="x" alt="x" />
-      <h2 data-testid="recipe-title">Título</h2>
       <button
         data-testid="share-btn"
         type="button"
       >
-        Compartilhar
+        <img src={ shareIcon } alt="shareIcon.svg" />
       </button>
       <button
         data-testid="favorite-btn"
         type="button"
       >
-        Favoritos
-      </button>
-      <h3 data-testid="recipe-category">Texto da categoria</h3>
-      <p data-testid="instructions">instruçoes</p>
-      <button
-        data-testid="finish-recipe-btn"
-        type="button"
-      >
-        Finalizar
+        <img src={ whiteHeartIcon } alt="whiteHeartIcon.svg" />
       </button>
       {recipe && (
         <div>
           {category !== 'Drink'
             ? <p data-testid="recipe-category">{recipe.strCategory}</p>
             : <p data-testid="recipe-category">{recipe.strAlcoholic}</p>}
-          <p data-testid="recipe-title">{recipe[`str${category}`]}</p>
+          <h3 data-testid="recipe-title">{recipe[`str${category}`]}</h3>
           <img
             width="150px"
             data-testid="recipe-photo"
             src={ recipe[`str${category}Thumb`] }
             alt=""
           />
-          { category !== 'Drink' && <iframe
-            data-testid="video"
-            src={ `https://www.youtube.com/embed/${getVideoId()}` }
-            title="recipe video"
-          />}
-          {getIngredients().map((ingredient, index) => (
+          {ingred.slice(0, indexNull).map((ingredient, index) => (
             <div key={ index }>
               <label htmlFor="recepi" data-testid={ `${index}-ingredient-step` }>
                 <input type="checkbox" />
@@ -82,9 +78,19 @@ function RecipeInProgress() {
             </div>
           ))}
           <p data-testid="instructions">{recipe.strInstructions}</p>
+          { category !== 'Drink' && <iframe
+            data-testid="video"
+            src={ `https://www.youtube.com/embed/${getVideoId()}` }
+            title="recipe video"
+          />}
+          <button
+            data-testid="finish-recipe-btn"
+            type="button"
+          >
+            Finalizar
+          </button>
         </div>
       )}
-      <Footer />
     </div>
   );
 }
