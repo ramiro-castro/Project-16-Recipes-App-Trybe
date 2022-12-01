27a2fa7afa8b4dc02b064ca666a4444edac787e2
Thiago Omoto <thi.omoto@gmail.com>
2022-12-01 15:13:21 -0300

    requisito 37 arrumado e parte do 38

--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -10,7 +10,7 @@ function RecipeInProgress() {
 
   useEffect(() => {
     if (history.location.pathname.includes('meals')) {
-      fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52882')
+      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${match.params.id}`)
         .then((res) => res.json())
         .then((data) => {
           setRecipe(data.meals[0]);
@@ -23,6 +23,12 @@ function RecipeInProgress() {
       setCategory('Drink');
     }
   }, []);
+
+  const getVideoId = () => recipe.strYoutube.split('v=')[1];
+  const getIngredients = () => Object.entries(recipe)
+    .filter((key) => key[0].includes('strIngredient')).map((ingredient) => ingredient[1]);
+  const getMeasures = () => Object.entries(recipe)
+    .filter((key) => key[0].includes('strMeasure')).map((ingredient) => ingredient[1]);
 
   console.log(recipe);
 
@@ -50,6 +56,34 @@ function RecipeInProgress() {
       >
         Finalizar
       </button>
+      {recipe && (
+        <div>
+          {category !== 'Drink'
+            ? <p data-testid="recipe-category">{recipe.strCategory}</p>
+            : <p data-testid="recipe-category">{recipe.strAlcoholic}</p>}
+          <p data-testid="recipe-title">{recipe[`str${category}`]}</p>
+          <img
+            width="150px"
+            data-testid="recipe-photo"
+            src={ recipe[`str${category}Thumb`] }
+            alt=""
+          />
+          { category !== 'Drink' && <iframe
+            data-testid="video"
+            src={ `https://www.youtube.com/embed/${getVideoId()}` }
+            title="recipe video"
+          />}
+          {getIngredients().map((ingredient, index) => (
+            <div key={ index }>
+              <label htmlFor="recepi" data-testid={ `${index}-ingredient-step` }>
+                <input type="checkbox" />
+                {`${ingredient} ${getMeasures()[index]}`}
+              </label>
+            </div>
+          ))}
+          <p data-testid="instructions">{recipe.strInstructions}</p>
+        </div>
+      )}
       <Footer />
     </div>
   );
